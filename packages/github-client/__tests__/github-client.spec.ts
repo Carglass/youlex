2f48d77715075874303d17a9506de104cf9a76be
Maxime Burlat <maximeburlat@gmail.com>
2019-04-03 16:35:26 -0500

    finally fixed jest, need to work on github client now

--- packages/github-client/__tests__/github-client.spec.ts
+++ packages/github-client/__tests__/github-client.spec.ts
@@ -4,14 +4,21 @@ const githubClient = require("..");
 
 describe("Environment is correctly setup", () => {
   test("environment is correctly setup", () => {
-    expect(process.env.APP_ID).toBe(27814);
+    expect(process.env.APP_ID).toBe("27814");
   });
 });
 
 describe("CRUD on the repos/laws works", () => {
   test("The repos are found correctly", async () => {
     expect.assertions(1);
-    const { data } = await githubClient.githubClient();
+    const { data } = await githubClient.lawRepo.get();
     expect(data).toBeTruthy();
   });
+
+  // test("creating a repo works", async () => {
+  //   expect.assertions(1);
+  //   const requestResult = await githubClient.lawRepo.create("Test Law", "M.Burlat");
+  //   const {data} = await githubClient.lawRepo.get();
+  //   expect(data.)
+  // })
 });
--- packages/github-client/src/github-client.ts
+++ packages/github-client/src/github-client.ts
@@ -4,28 +4,6 @@ const env = config();
 const App = require("@octokit/app");
 const Octokit = require("@octokit/rest");
 
-// code usefule to find an installation id
-// reading a file is unnecessary though
-// const privateKey = readFileSync(
-//   "./lib/youlex-github.2019-03-28.private-key (2).pem"
-// );
-// console.log(privateKey);
-// const app = new App({
-//   id: process.env.APP_ID,
-//   privateKey: privateKey
-// });
-// const token = app.getSignedJsonWebToken();
-// console.log(token);
-// const octokit = new Octokit({
-//   auth() {
-//     return `bearer ${token}`;
-//   }
-// });
-
-// octokit.apps.listInstallations().then(result => {
-//   console.log(result);
-// });
-
 const app = new App({
   id: process.env.APP_ID,
   privateKey: process.env.PRIVATE_KEY
